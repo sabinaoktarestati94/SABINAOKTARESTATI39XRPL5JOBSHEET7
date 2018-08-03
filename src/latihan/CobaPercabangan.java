906fdd7f5897bece30480273cf24df770073ac50
Asus <Asus@DESKTOP-B57PKP8>
2018-08-03 11:18:01 +0700

    Coba IF..ELSE.

--- src/latihan/CobaPercabangan.java
+++ src/latihan/CobaPercabangan.java
@@ -31,7 +31,12 @@ public class CobaPercabangan {
         if(a > 5) {
             System.out.println("isi variabel a : "+ a);
             System.out.println("nilai a lebih dari 5");
+            System.out.println("\nIF..ELSE..");
+            if(a<7) {
+                System.out.println("isi variabel a : "+a);
+                System.out.println("nilai a kurang dari 5");
+            } else System.out.println("nilai a lebih dari atau sama dengan5");
         }
     }
-}
+            }
 
