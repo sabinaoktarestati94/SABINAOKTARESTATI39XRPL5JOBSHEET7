067163ae683ec4f5b8df2a1405ca39e1c8d57c38
Asus <Asus@DESKTOP-B57PKP8>
2018-08-03 11:06:55 +0700

    Coba IF

--- src/latihan/CobaPercabangan.java
+++ src/latihan/CobaPercabangan.java
@@ -5,6 +5,8 @@
  */
 package latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Asus
@@ -13,5 +15,23 @@ public class CobaPercabangan {
     public static void main(String[]args) {
         String identitas = "Sabina Okta Restati / XRPL5 / 39";
         System.out.println("Identitas : "+ identitas);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+                System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5) {
+            System.out.println("isi variabel a : "+ a);
+            System.out.println("nilai a lebih dari 5");
+        }
     }
 }
+
