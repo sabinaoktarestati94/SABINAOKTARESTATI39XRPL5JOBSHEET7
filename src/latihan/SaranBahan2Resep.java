edc83f17114e30109adec0b053ce800949df2613
Asus <Asus@DESKTOP-B57PKP8>
2018-08-03 17:58:40 +0700

    Tambah Input Bahan 1

--- src/latihan/SaranBahan2Resep.java
+++ src/latihan/SaranBahan2Resep.java
@@ -1,3 +1,7 @@
+package latihan;
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,4 +19,12 @@ public class SaranBahan2Resep {
     
     System.out.print("nSaran Resep dari Bahan Milik Anda\n");
     
+        Scanner Scanner = new Scanner(System.in);
+    
+    System.out.println("Bahan Pertama: ");
+    System.out.println("1. Pisang");
+    System.out.println("2. Telur");
+    System.out.print("Masukkan no pilihan Anda : ");
+    int bahan1 = scanner.nextInt();
+    }
 }
