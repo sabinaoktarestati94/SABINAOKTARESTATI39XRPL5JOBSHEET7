75efa78d966a82317caef37f6559a9c382488add
Asus <Asus@DESKTOP-B57PKP8>
2018-08-03 12:49:47 +0700

    Coba Nested IF.

--- src/latihan/CobaPercabangan.java
+++ src/latihan/CobaPercabangan.java
@@ -55,9 +55,17 @@ public class CobaPercabangan {
                 System.out.println("nilai a sama dengan 6");
             } else {
                 System.out.println("nilai a lebih dari 6");
+            System.out.println("\nNested IF");
+            if(a < 7) {
+                System.out.println("nilai a kurang dari 7");
+                if(a > 2)
+                    System.out.println("nilai a lebih dari 2");
+                if(a < 4)
+                    System.out.println("nilai a kurang dari 4");
+            }
+            }
             }
             }
         }
-    }
-            
+    
 
